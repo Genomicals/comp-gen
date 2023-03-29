dcfcb2c49115db636aa9fe0b06f1beca80ca81a6
nbalcarc <nathan.balcarcel@wsu.edu>
2023-03-29 16:04:23 -0700

    Removed target and updated .gitignore

--- suffix-tree/target/debug/deps/suffix_tree-804ad36daa217f44.d
+++
@@ -1,5 +0,0 @@
-/home/terrior/Programming/comp-gen/suffix-tree/target/debug/deps/suffix_tree-804ad36daa217f44.rmeta: src/main.rs
-
-/home/terrior/Programming/comp-gen/suffix-tree/target/debug/deps/suffix_tree-804ad36daa217f44.d: src/main.rs
-
-src/main.rs:
--- suffix-tree/target/debug/suffix-tree.d
+++
@@ -1,1 +0,0 @@
-/home/terrior/Programming/comp-gen/suffix-tree/target/debug/suffix-tree: /home/terrior/Programming/comp-gen/suffix-tree/src/main.rs
